a83c03ba889145671e1ed3f71505113b5c88a7ba
Ramesh R <ramesh.rahu96@gmail.com>
2024-09-29 23:44:26 +0530

    initial commit

---
+++ R/correlation.R
@@ -0,0 +1,250 @@
+# Load required libraries
+library(shiny)
+library(corrplot)
+library(stats)
+library(ggcorrplot)
+library(readxl)
+library(openxlsx)
+library(DT)
+library(officer)
+library(flextable)
+library(bslib)
+
+# Define UI
+ui <- fluidPage(
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "simplex",
+    primary = "#007bff",
+    "font-size-base" = "0.95rem"
+  ),
+  tags$head(
+    tags$style(HTML("
+      h1 {
+        text-align: center;
+      }
+    "))
+  ),
+  h1("Correlation Analysis",
+     style = "font-family:Times New Roman; font-weight: bold; font-size: 36px; color: #4CAF50;"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      position="right",
+      fluid=TRUE,
+      actionButton("clipboard_input", "Paste from Clipboard"),
+      fileInput("file", "Or choose a file (CSV or Excel)", accept = c(".csv", ".xlsx")),
+      selectInput("method", "Correlation Method:",
+                  choices = c("pearson", "kendall", "spearman")),
+      actionButton("analyze", "Analyze",style = "color: royalblue; font-size: 20px;"),
+      hr(),
+      selectInput("font_family", "Choose Font Style:", 
+                  choices = c("serif", "sans", "mono", "Arial", "Helvetica", "Times New Roman")),
+      numericInput("font_size", "Font Size:", value = 12, min = 8, max = 20),
+      selectInput("color_palette", "Choose Color Palette:",
+                  choices = c("RdYlBu", "RdBu", "PuOr", "PRGn", "BrBG", "PiYG")),
+      numericInput("plot_width", "Plot Width (inches):", value = 10, min = 1, max = 20),
+      numericInput("plot_height", "Plot Height (inches):", value = 8, min = 1, max = 20),
+      numericInput("plot_dpi", "Plot DPI:", value = 300, min = 72, max = 600),
+      downloadButton("download_plot", "Download Plot",class = "btn-info"),
+      downloadButton("download_word", "Download Word Report",class = "btn-info"),
+      fluidPage(
+        fluidRow(
+          column(9, offset = 1,  # This shifts the column 4 spaces to the right
+                 card(
+                   height = "auto",
+                   card_header(
+                     div(
+                       class = "text-center",
+                       "visvaR- VISualize VARiance"
+                     ),
+                     class = "bg-primary text-white"
+                   ),
+                   HTML("
+        <p style='text-align: center;'>
+          <strong>Developed by</strong><br>
+          <span style='font-size: 24px;'>Ramesh R</span><br>
+          <span>PhD Scholar</span><br>
+          <span>Division of Plant Physiology </span><br> ICAR-IARI, New Delhi</span><br>
+          <span>ramesh.rahu96@gmail.com</span>
+        </p>
+        "),
+                   card_image("visvaR.png",
+                              style = "display: block; margin-left: auto; margin-right: auto;",
+                              alt = "",
+                              src = NULL,
+                              href = NULL,
+                              border_radius = c("auto"),
+                              mime_type = NULL,
+                              class = NULL,
+                              height ="30%",
+                              fill = FALSE,
+                              width = "30%",
+                              container = NULL
+                   )
+                 )
+          )
+        ),
+        
+      ),
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Data Preview", DTOutput("data_preview")),
+        tabPanel("Correlation Plot", plotOutput("correlation_plot", height = "600px")),
+        tabPanel("Correlation Matrix", DTOutput("correlation_matrix"))
+      )
+    )
+  )
+)
+# Define server logic
+server <- function(input, output, session) {
+  
+  data <- reactiveVal(NULL)
+  correlation_matrix <- reactiveVal(NULL)
+  original_names <- reactiveVal(NULL)
+  
+  observeEvent(input$clipboard_input, {
+    tryCatch({
+      df <- read.delim("clipboard", header = TRUE, check.names = FALSE)
+      data(df)
+      original_names(colnames(df))
+      showNotification("Data successfully read from clipboard", type = "message")
+    }, error = function(e) {
+      showNotification("Error reading from clipboard. Please try again.", type = "error")
+    })
+  })
+  
+  observeEvent(input$file, {
+    req(input$file)
+    ext <- tools::file_ext(input$file$name)
+    
+    tryCatch({
+      if (ext == "csv") {
+        df <- read.csv(input$file$datapath, header = TRUE, check.names = FALSE)
+      } else if (ext == "xlsx") {
+        df <- read_excel(input$file$datapath, col_names = TRUE)
+      } else {
+        stop("Unsupported file format")
+      }
+      data(df)
+      original_names(colnames(df))
+      showNotification("File successfully uploaded", type = "message")
+    }, error = function(e) {
+      showNotification(paste("Error reading file:", e$message), type = "error")
+    })
+  })
+  
+  output$data_preview <- renderDT({
+    req(data())
+    datatable(data(), options = list(scrollX = TRUE, scrollY = "400px", pageLength = 15))
+  })
+  
+  observeEvent(input$analyze, {
+    req(data())
+    M <- cor(data(),method = input$method)
+    correlation_matrix(M)
+  })
+  
+  create_correlation_plot <- function() {
+    req(correlation_matrix())
+    
+    M <- correlation_matrix()
+    res1 <- cor.mtest(data(), conf.level = 0.95)
+    
+    par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1, family = input$font_family)
+    
+    corrplot(M, p.mat = res1$p, order = "AOE", type = "upper", tl.pos = "lt", tl.col = "black",
+             insig = "label_sig", sig.level = c(0.001, 0.01, 0.05), pch.cex = 2, pch.col = "black",
+             tl.cex = input$font_size / 12, cl.cex = input$font_size / 12, tl.srt = 45, 
+             col = COL2(input$color_palette, 10),
+             tl.offset = 0.5)
+    
+    corrplot(M, add = TRUE, type = "lower", method = "number", col = "black",
+             number.cex = input$font_size / 12, order = "AOE", diag = FALSE,
+             tl.pos = "n", cl.pos = "n", tl.cex = input$font_size / 12, tl.srt = 45)
+  }
+  
+  output$correlation_plot <- renderPlot({
+    create_correlation_plot()
+  })
+  
+  output$correlation_matrix <- renderDT({
+    req(correlation_matrix())
+    matrix_with_names <- correlation_matrix()
+    colnames(matrix_with_names) <- original_names()
+    rownames(matrix_with_names) <- original_names()
+    datatable(round(matrix_with_names, 2), options = list(scrollX = TRUE, scrollY = "400px"))
+  })
+  
+  output$download_plot <- downloadHandler(
+    filename = function() {
+      paste("correlation_plot_", Sys.Date(), ".png", sep = "")
+    },
+    content = function(file) {
+      png(file, width = input$plot_width, height = input$plot_height, units = "in", res = input$plot_dpi)
+      create_correlation_plot()
+      dev.off()
+    }
+  )
+  
+  output$download_word <- downloadHandler(
+    filename = function() {
+      paste("correlation_analysis_report_", Sys.Date(), ".docx", sep = "")
+    },
+    content = function(file) {
+      doc <- read_docx()
+      fp_bold <- fp_text_lite(bold = TRUE)
+      fp_refnote <- fp_text_lite(vertical.align = "baseline")
+      bl <- block_list(
+        fpar(ftext("Analysis done using visvaR R package ", fp_bold)),
+        fpar(
+          ftext("Developed by Ramesh R PhD scholar, Division of Plant Physiology, ICAR-IARI, New Delhi", fp_bold))
+      )
+      a_par <- fpar(
+        "visvaR- Visulaize Variance                                           ",
+        run_footnote(x = bl, prop = fp_refnote),
+        ""
+      )
+      
+      doc <- body_add_fpar(doc, value = a_par, style = "Normal")
+      
+      set_flextable_defaults(
+        font.size = 12, font.family = "Times New Roman",
+        font.color = "#333333",
+        table.layout = "autofit",
+        border.color = "black")
+      # Add title
+      doc <- doc %>%
+        body_add_par("Correlation Analysis Report", style = "heading 1")
+      # Add correlation plot
+      temp_plot <- tempfile(fileext = ".png")
+      png(temp_plot, width = input$plot_width, height = input$plot_height, units = "in", res = input$plot_dpi)
+      create_correlation_plot()
+      dev.off()
+      
+      doc <- doc %>%
+        body_add_par("Correlation Plot", style = "heading 2") %>%
+        body_add_img(src = temp_plot, width = 6, height = 6)
+      
+      # Add correlation matrix
+      doc <- doc %>%
+        body_add_par("Correlation Matrix", style = "heading 2")
+      
+      matrix_with_names <- correlation_matrix()
+      colnames(matrix_with_names) <- original_names()
+      rownames(matrix_with_names) <- original_names()
+      matrix_table <- flextable(round(as.data.frame(matrix_with_names), 2))
+      matrix_table <- autofit(matrix_table)
+      matrix_table<-add_footer_lines(matrix_table,paste("Method used:", input$method))
+      doc <- doc %>%
+        body_add_flextable(matrix_table)
+      
+      print(doc, target = file)
+    }
+  )
+}
+
+# Run the Shiny app
+shinyApp(ui = ui, server = server)
---
+++ R/oneway_rbd.R
@@ -0,0 +1,337 @@
+library(shiny)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(agricolae)
+library(flextable)
+library(officer)
+library(tibble)
+library(tidyr)
+library(bslib)
+library(patchwork)
+
+ui <- page_fluid(
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "simplex",
+    primary = "#007bff",
+    "font-size-base" = "0.95rem"
+  ),
+  tags$head(
+    tags$style(HTML("
+      h1 {
+        text-align: center;
+      }
+    "))
+  ),
+  
+  # Center-aligned title
+  h1("One-way Randomized Comlete Block Design (RCBD)",
+     style = "font-family:Times New Roman; font-weight: bold; font-size: 36px; color: #890304;"),
+  layout_columns(
+    col_widths = c(3, 9),
+    card( height = "45",
+          card_header(
+            div(
+              class = "text-center",
+              "visvaR- VISualize VARiance"
+            ),
+            class = "bg-primary text-white"
+          ),
+          HTML("
+    <p style='text-align: center;'>
+      <strong>Developed by</strong><br>
+      <span style='font-size: 24px;'>Ramesh R</span><br>
+      <span>PhD Scholar</span><br>
+      <span>Division of Plant Physiology </span><br> ICAR-IARI, New Delhi</span><br>
+      <span>ramesh.rahu96@gmail.com</span>
+    </p>
+  "),
+          card_image("visvaR.png",
+                     style = "display: block; margin-left: auto; margin-right: auto;",
+                     alt = "",
+                     src = NULL,
+                     href = NULL,
+                     border_radius = c("auto"),
+                     mime_type = NULL,
+                     class = NULL,
+                     height ="50%",
+                     fill = FALSE,
+                     width = "50%",
+                     container = NULL
+          )
+    ),
+    card( height = "auto",
+          card_header( 
+            div( class="text-center",
+                 "Data Input and Analysis Options",
+                 height= "120px",
+                 class = "bg-primary text-white"
+            )),
+          card_body(
+            fluidRow(
+              fileInput("file", "Choose .xlsx or .csv", accept = c(".xlsx", ".csv")),
+              actionButton("clipboard_button","Use Clipboard Data", class = "text-black",
+                           style = "width: 700px; height:35px; float:center;margin-top: 30px;",
+                           title="Click here to paste your clipboard data"),
+              strong("Choose acccording to your need", align='center'),
+              selectInput("test_method", "Choose Post-Hoc Test",
+                          choices = c("LSD" = "LSD.test", 
+                                      "Duncan" = "duncan.test",
+                                      "Tukey HSD" = "HSD.test",
+                                      "Student-Newman-Keuls" = "SNK.test")),
+              textInput("bar_color", "Bar Color (name or hex code)", value = "coral1"),
+              selectInput("font_family", "Font Style for plot", 
+                          choices = c("Sans" = "sans", "Serif" = "serif", "Mono" = "mono", "Times New Roman" = "Times New Roman")),
+              numericInput("font_size", "Font Size for plot", value = 12, min = 8, max = 20),
+              textInput("output_filename", "Output Filename", value = "result"),
+              actionButton("analyze_button", "Analyze", class = "btn-success"),
+              strong("Downlod and save your results in document format by cliclking link below", align='center'),
+              downloadButton("download_report", "Download Report", class = "btn-info")
+            )
+          )
+    )
+  ),
+  layout_columns(
+    col_widths = c(12),
+    card(
+      card_header(
+        "Results",
+        class = "bg-primary text-white"
+      ),
+      card_body(
+        tabsetPanel(
+          tabPanel("Data Preview", DT::DTOutput("preview")),
+          tabPanel("", 
+                   uiOutput("analysis_outputs")
+          )
+        ),
+        uiOutput("analysis_status")  # Add this line to display the analysis status
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  data <- reactiveVal(NULL)
+  report <- reactiveVal(NULL)
+  analysis_complete <- reactiveVal(FALSE)  # Add this line
+  # Add this to create the analysis status UI
+  output$analysis_status <- renderUI({
+    if (analysis_complete()) {
+      div( class="text-center",
+        style = "margin-top: 20px; text-align: center;",
+        tags$strong("Analysis complete!", style = "color: green; font-size: 30px;")
+      )
+    }
+  })
+  observeEvent(input$file, {
+    req(input$file)
+    data(read_excel(input$file$datapath))
+  })
+  
+  observeEvent(input$clipboard_button, {
+    clipboard_data <- read.delim("clipboard", header = TRUE)
+    data(clipboard_data)
+  })
+  
+  output$preview <- DT::renderDT({
+    req(data())
+    data()
+  })
+  
+  observeEvent(input$analyze_button, {
+    req(data())
+    
+    # Reset the analysis_complete status
+    analysis_complete(FALSE)
+    
+    # Show a notification that analysis has started
+    showNotification("Analysis in progress...", type = "message", duration = NULL, id = "analysis_notification")
+    
+    data_full <- data()
+    factor_col <- colnames(data_full)[1]
+    rep_col <- colnames(data_full)[2]
+    response_cols <- colnames(data_full)[3:ncol(data_full)]
+    
+    doc <- read_docx()
+
+    for (i in seq_along(response_cols)) {
+      response_col <- response_cols[i]
+      
+      data1 <- data_full %>%
+        select(all_of(c(factor_col, rep_col, response_col))) %>%
+        rename(Factor_A = 1, Replication = 2, Response = 3)
+      
+      data1$Factor_A <- as.factor(data1$Factor_A)
+      data1$Replication <- as.factor(data1$Replication)
+      
+      anova_model <- aov(Response ~ Factor_A + Replication, data = data1)
+      aov_result <- anova(anova_model)
+      
+      fitted_values <- fitted(anova_model)
+      residuals <- resid(anova_model)
+      
+      res_fit_plot <- ggplot(data = data.frame(Fitted = fitted_values, Residuals = residuals), aes(x = Fitted, y = Residuals)) +
+        geom_point() +
+        geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+        labs(title = paste("Residuals vs Fitted -", response_col), x = "Fitted Values", y = "Residuals") +
+        theme_minimal() +
+        theme(text = element_text(family = input$font_family, size = input$font_size))
+      
+      qq_plot <- ggplot(data = data.frame(Sample = residuals), aes(sample = Sample)) +
+        stat_qq() +
+        stat_qq_line() +
+        labs(title = paste("Q-Q Plot -", response_col), x = "Theoretical Quantiles", y = "Sample Quantiles") +
+        theme_minimal() +
+        theme(text = element_text(family = input$font_family, size = input$font_size))
+      combined_plot <- res_fit_plot + qq_plot + plot_layout(nrow = 2) 
+      perform_test <- function(response, treatment, DFerror, MSerror) {
+        test_function <- get(input$test_method)
+        test_function(y = response,
+                      trt = treatment,
+                      DFerror = DFerror,
+                      MSerror = MSerror,
+                      alpha = 0.05,
+                      group = TRUE,
+                      console = TRUE)}
+      
+      MeanSE_A <- data1 %>%
+        group_by(Factor_A) %>%
+        summarise(avg_A = mean(Response),
+                  se = sd(Response)/sqrt(length(Response)))
+      LSD_A <- perform_test(data1$Response, data1$Factor_A, anova_model$df.residual, deviance(anova_model)/anova_model$df.residual)
+      
+      ascend_A <- LSD_A$groups %>%
+        group_by(rownames(LSD_A$groups)) %>%
+        arrange(rownames(LSD_A$groups))
+      
+      main_plot <- ggplot(MeanSE_A, aes(x = Factor_A, y = avg_A, fill = Factor_A)) +
+        geom_bar(stat = "identity", color = "black",
+                 width = 0.5, position = position_dodge(width=0.5)) +
+        geom_errorbar(aes(ymax = avg_A + se, ymin = avg_A - se),
+                      position = position_dodge(width=0.1), width = 0.1) +
+        theme(panel.background = element_rect(fill="white", colour="white", linetype="solid", color="white"),
+              legend.position = "none",
+              axis.text.x = element_text(angle = 0, face = "bold"),
+              panel.border = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.line = element_line(linewidth = 0.5, linetype = "solid", colour = "black"),
+              text = element_text(family = input$font_family, size = input$font_size)) +
+        labs(title = paste("", response_col), x = factor_col, y = response_col,color = "factor_col") +
+        geom_text(aes(x = Factor_A, y = avg_A + se, label = as.matrix(ascend_A$groups)),
+                  position = position_dodge(width = 0.9), vjust = -(0.5)) +
+        scale_fill_manual(values = rep(input$bar_color, length(unique(MeanSE_A$Factor_A))))+
+        scale_y_continuous(expand = c(0,0))+
+        expand_limits(y = c(0, max(MeanSE_A$avg_A+ (MeanSE_A$avg_A*0.25))))+
+        labs(color = NULL) 
+      
+      
+      # Generate Word document content
+      aov_df <- data.frame(Source = rownames(aov_result), aov_result)
+      aov_df[, "Significance"] <- ifelse(aov_df$Pr..F. <= 0.001, "***",
+                                         ifelse(aov_df$Pr..F. <= 0.01, "**",
+                                                ifelse(aov_df$Pr..F. <= 0.05, "*", 
+                                                      ifelse(aov_df$Pr..F. <= 0.1, ".", " "))))
+      
+      fp_bold <- fp_text_lite(bold = TRUE)
+      fp_refnote <- fp_text_lite(vertical.align = "baseline")
+      bl <- block_list(
+        fpar(ftext("Analysis done using visvaR R package ", fp_bold)),
+        fpar(
+          ftext("Developed by Ramesh R PhD scholar, Division of Plant Physiology, ICAR-IARI, New Delhi", fp_bold))
+      )
+      a_par <- fpar(
+        "visvaR- Visulaize Variance                                           ",
+        run_footnote(x = bl, prop = fp_refnote),
+        ""
+      )
+      
+      doc <- body_add_fpar(doc, value = a_par, style = "Normal")
+      
+      set_flextable_defaults(
+        font.size = 12, font.family = "Times New Roman",
+        font.color = "#333333",
+        table.layout = "autofit",
+        border.color = "black")
+      
+      ft <- flextable(aov_df)
+      ft <- set_caption(ft, caption = paste("ANOVA Results -", response_col))
+      ft <- add_footer_lines(ft, values = "Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
+      ft <- set_table_properties(ft, width = 0.9, layout = "autofit")
+      
+      colnames(ascend_A)[3] <- "Factor_A"
+      combined_df <- merge(ascend_A, MeanSE_A, by = "Factor_A")
+      ft_m <- flextable(combined_df)
+      ft_m <- set_caption(ft_m, caption = paste("Mean Comparison -", response_col))
+      ft_m <- set_table_properties(ft_m, width = 0.75, layout = "autofit")
+      ft_m<-add_footer_lines(ft_m,paste("Post-hoc test used:", input$test_method))
+      
+      ft_cv<-flextable(LSD_A$statistics)
+      ft_cv<- set_caption(ft_cv, caption=paste("ANOVA Stats -", response_col))
+      ft_cv <- set_table_properties(ft_cv, width = 0.75, layout = "autofit")
+      doc <- body_add_flextable(doc, ft)
+      doc <- body_add_flextable(doc, ft_m)
+      doc <- body_add_flextable(doc, ft_cv)
+      doc <- body_add_gg(doc, main_plot, width = 6, height = 4)
+      doc <- body_add_gg(doc, combined_plot, width = 4, height = 4)
+      doc<-body_add_break(doc,pos = "after")
+      
+      # Create dynamic UI outputs
+      output[[paste0("analysis_output_", i)]] <- DT::renderDT({
+        print(aov_result)
+      })
+      output[[paste0("analysis_output_", i)]] <- renderPrint({
+        cat("Post-hoc test used:", input$test_method, "\n\n")
+        print(aov_result)
+      })
+      
+      output[[paste0("plot_", i)]] <- renderPlot({
+        main_plot
+      })
+      
+      output[[paste0("combined_plot", i)]] <- renderPlot({
+        res_fit_plot
+      })
+    }
+    
+    # Store the generated document in the reactive value
+    report(doc)
+    
+    # Update UI with dynamic outputs
+    output$analysis_outputs <- renderUI({
+      lapply(seq_along(response_cols), function(i) {
+        tagList(
+          h3(paste("Analysis for", response_cols[i])),
+          verbatimTextOutput(paste0("analysis_output_", i)),
+          plotOutput(paste0("plot_", i)),
+          plotOutput(paste0("combined_plot", i)),
+          hr()
+        )
+      })
+    })
+    
+    # Set analysis_complete to TRUE
+    analysis_complete(TRUE)
+    
+    # Remove the "in progress" notification and show a completion notification
+    removeNotification(id = "analysis_notification")
+    showNotification("Analysis complete!", type = "message", duration = 20)
+  })
+  
+  
+  
+  # Download Handler for the report
+  output$download_report <- downloadHandler(
+    filename = function() {
+      paste0(input$output_filename, ".docx")
+    },
+    content = function(file) {
+      req(report())
+      print(report(), target = file)
+    }
+  )
+}
+
+shinyApp(ui, server)
